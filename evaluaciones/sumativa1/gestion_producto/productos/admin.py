261a602728d8d63f4bf196058b3b72d3eae84eaa
shajunior <andrejunior931993@gmail.com>
2024-10-14 21:55:05 -0300

    avance

--- evaluaciones/sumativa1/gestion_producto/productos/admin.py
+++ evaluaciones/sumativa1/gestion_producto/productos/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Categoria, Marca, Caracteristica, Producto
 
-# Register your models here.
+admin.site.register(Categoria)
+admin.site.register(Marca)
+admin.site.register(Caracteristica)
+admin.site.register(Producto)
--- evaluaciones/sumativa1/gestion_producto/productos/models.py
+++ evaluaciones/sumativa1/gestion_producto/productos/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+class Marca(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+class Caracteristica(models.Model):
+    tipo = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.tipo
+
+class Producto(models.Model):
+    codigo = models.CharField(max_length=10)
+    nombre = models.CharField(max_length=100)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
+    caracteristicas = models.ManyToManyField(Caracteristica, through="Enlace")
+
+    def __str__(self):
+        return f'{self.nombre} ({self.codigo})'
+    
+class Enlace(models.Model):
+    caracteristica = models.ForeignKey(Caracteristica, null=False, on_delete=models.CASCADE)
+    producto = models.ForeignKey(Producto, null=False, on_delete=models.CASCADE)
